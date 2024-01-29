611f4f953fea2a49aa55064b00d7296e493e2826
Ryan Bennett <nomad.ry@gmail.com>
2024-01-29 15:08:46 -0700

    comment demo

--- demo.mjs
+++ demo.mjs
@@ -5,23 +5,39 @@ const workflow = new Workflow("program-1", {
 });
 
 workflow
-    .addNode("makeHaiku", "write a haiku")
+    // setup nodes
+    .addNode("makeHaiku", "write a haiku", {
+        model: "gpt-3.5-turbo-16k", // defaults to gpt-4-turbo-preview
+        temperature: 0.2, // defaults to 0.3
+        role: "user", // defaults to "user"
+    })
     .addNode(
         "rateHaiku",
-        "Pretend to be a haiku judge and provide a rating from 1 to 10. You MUST provide a number. This is an exercise in testing branching code to detect your rating, dont over think it. If the haiku has gone through more than 3 revisions, just give it a 10. repeat the haiku before your rating"
+        [
+            "Pretend to be a haiku judge and provide a rating from 1 to 10.",
+            "You MUST provide a number.",
+            "This is an exercise in testing branching code to detect your rating, dont over think it.",
+            "If the haiku has gone through more than 3 revisions, just give it a 10.",
+            "repeat the haiku before your rating.",
+        ].join("\n")
     )
     .addNode("improveHaiku", "please improve the haiku")
     .addNode(
         "outputSuccess",
         "please restate the most recent haiku and its rating."
     )
+    // connect our nodes
     .connect("makeHaiku", "rateHaiku")
     .connect("rateHaiku", "improveHaiku", "is the rating less than a 10?")
     .connect("improveHaiku", "rateHaiku")
     .connect("rateHaiku", "outputSuccess", "is the rating exactly 10?")
+    // prefix path to output completed runs
     .output(`./testsdk`)
+    // log event streams to stdout
     .log();
 
-const { value, $ } = await workflow.execute("I like space");
+// execute a flow with a first prompt (optional)
+const { value } = await workflow.execute("I like space");
+//const { value } = await workflow.execute() <-- this would just get you a random haiku.
 
 console.log("workflow complete", value);
