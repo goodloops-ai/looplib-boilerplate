0e7cd503a3d5de3d55ae1495b817b04237ed2fdf
Ryan Bennett <nomad.ry@gmail.com>
2024-02-10 12:20:44 -0700

    bail on failed tests

--- examples/alphacode/testworker.mjs
+++ examples/alphacode/testworker.mjs
@@ -1,50 +1,46 @@
 self.onmessage = async (event) => {
-    const { challenge, types, src } = event.data;
+    const { challenge, types, src, breakOnFailure = true } = event.data;
 
     try {
         const module = await import(src);
+        const results = {};
 
-        const results = types.reduce(
-            (acc, type, index) => ({
-                ...acc,
-                [type]: challenge[type].input.reduce(
-                    (res, inputStr, index) => {
-                        try {
-                            const inputLines = inputStr.trim().split("\n");
-                            // console.log("START TEST");
-                            const output = module
-                                .default(inputLines)
-                                .join("\n")
-                                .trim();
-                            // console.log("END TEST");
-                            const compare =
-                                challenge[type].output[index].trim();
-                            if (output == compare) {
-                                return {
-                                    ...res,
-                                    pass: res.pass + 1,
-                                };
-                            } else {
-                                return {
-                                    ...res,
-                                    fail: res.fail + 1,
-                                };
-                            }
-                        } catch (e) {
-                            return {
-                                ...res,
-                                fail: res.fail + 1,
-                            };
+        for (const type of types) {
+            let pass = 0;
+            let fail = 0;
+
+            for (const index in challenge[type].input) {
+                try {
+                    const inputStr = challenge[type].input[index];
+                    const inputLines = inputStr.trim().split("\n");
+                    const output = module.default(inputLines).join("\n").trim();
+                    const compare = challenge[type].output[index].trim();
+
+                    if (output == compare) {
+                        pass++;
+                    } else {
+                        fail++;
+                        if (breakOnFailure) {
+                            break;
                         }
-                    },
-                    {
-                        pass: 0,
-                        fail: 0,
                     }
-                ),
-            }),
-            {}
-        );
+                } catch (e) {
+                    fail++;
+                    if (breakOnFailure) {
+                        break;
+                    }
+                }
+            }
+
+            results[type] = { pass, fail };
+
+            if (fail > 0) {
+                if (breakOnFailure) {
+                    break;
+                }
+            }
+        }
+
         self.postMessage(results);
     } catch (e) {
         self.postMessage({ error: e.toString() });
