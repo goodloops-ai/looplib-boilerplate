f7f56c37f7868b2c32f374ff23216eca3e7a4f26
Ryan Bennett <nomad.ry@gmail.com>
2024-04-25 14:40:55 -0600

    dcl fix with openrouter

--- dspl/codiumResults.mjs
+++ dspl/codiumResults.mjs
@@ -5,21 +5,21 @@ const raw = await Deno.readTextFile(report);
 const j = JSON.parse(raw);
 const challenges = JSON.parse(j.context.blackboard.challengesJSON);
 
+console.log(
+    "Public test errors:",
+    challenges
+        .map((c) =>
+            c.public_test_results.filter(
+                (r) => r.status !== "pass" && r.status !== "fail"
+            )
+        )
+        .filter((r) => r.length > 0)
+);
+
+// console.log(challenges);
 console.log("Total Challenges", challenges.length);
 console.log("Total Pass:", challenges.filter((c) => c.tests_passed).length);
 console.log(
     "Public Tests Passed",
     challenges.filter((c) => c.public_tests_passed).length
 );
-// console.log(
-//     "Private Tests Passed",
-//     challenges
-//         .map((c) =>
-//             c.public_test_results.filter(
-//                 (r) => r.status !== "pass" && r.status !== "fail"
-//             )
-//         )
-//         .filter((r) => r.length > 0)
-// );
-
-// console.log(challenges);
